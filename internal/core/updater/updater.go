6f2539392147e26eb374cf0901ba2266c16e45ec
yhoundz <43507714+yhoundz@users.noreply.github.com>
2025-12-21 23:06:52 -0600

    added pinning/unpinng of packages to prevent/allow updates

--- internal/core/updater/updater.go
+++ internal/core/updater/updater.go
@@ -3,11 +3,9 @@ package updater
 import (
 	"context"
 	"fmt"
-	"os"
 	"parm/internal/core/installer"
 	"parm/internal/gh"
 	"parm/internal/manifest"
-	"parm/internal/parmutil"
 	"parm/pkg/progress"
 
 	"github.com/Masterminds/semver/v3"
@@ -15,6 +13,7 @@ import (
 )
 
 // TODO: modify updater to use new symlinking logic
+// 12-21-25 ngl idk what ts means lol
 
 type Updater struct {
 	client    *github.RepositoriesService
@@ -38,24 +37,21 @@ func New(cli *github.RepositoriesService, rel *installer.Installer) *Updater {
 }
 
 // TODO: update concurrently?
-func (up *Updater) Update(ctx context.Context, owner, repo string, installPath string, flags *UpdateFlags, hooks *progress.Hooks) (*UpdateResult, error) {
-	installDir := parmutil.GetInstallDir(owner, repo)
-	man, err := manifest.Read(installDir)
-
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil, fmt.Errorf("package %s/%s does not exist", owner, repo)
-		}
-		return nil, fmt.Errorf("could not read manifest for %s/%s: %w", owner, repo, err)
-	}
-
+func (up *Updater) Update(ctx context.Context, owner, repo string, installPath string, man *manifest.Manifest, flags *UpdateFlags, hooks *progress.Hooks) (*UpdateResult, error) {
 	var rel *github.RepositoryRelease
+	var err error
 
 	switch man.InstallType {
 	case manifest.Release:
 		rel, _, err = up.client.GetLatestRelease(ctx, owner, repo)
+		if err != nil {
+			return nil, err
+		}
 	case manifest.PreRelease:
 		rel, err = gh.GetLatestPreRelease(ctx, up.client, owner, repo)
+		if err != nil {
+			return nil, err
+		}
 		// TODO: DRY @installer.go
 		if !flags.Strict {
 			// expensive!
